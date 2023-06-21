7c2926d0e2e6449cfd08581706c0a15cd2cb4fbe
xweetx <xweet_xilenze@yahoo.com>
2023-06-21 13:33:48 -0600

    update

--- index2.js
+++ index2.js
@@ -1,28 +1,18 @@
-const request = require('request-promise-native');
+const { nextISSTimesForMyLocation } = require("./iss_promised");
 
-const fetchMyIP = function() {
-  return request("https://api.ipify.org?format=json");
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
 };
 
-const fetchCoordsByIP = function(body) {
-  const stringIP = JSON.parse(body).ip;
-  return request(`https://ipvigilante.com/json/${stringIP}`);
-};
-
-const fetchISSFlyOverTimes = function(body) {
-  const { latitude, longitude } = JSON.parse(body).data;
-  const URL = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
-  return request(URL);
-};
-
-const nextISSTimesForMyLocation = function() {
-  return fetchMyIP()
-    .then(fetchCoordsByIP)
-    .then(fetchISSFlyOverTimes)
-    .then((data) => {
-      const { response } = JSON.parse(data);
-      return response;
-    });
-};
-
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation  };
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
